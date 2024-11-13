4fefc1b8fae48734f1af23b5ed3837dca3d7015c
genvil0317 <genvil0317@gmail.com>
2024-11-13 15:59:35 +0800

    program.py with error handling

--- program.py
+++ program.py
@@ -1,3 +1,9 @@
+try:
+    file = open("file1.txt", "x")
+    print("File Successfully Created.")
+except:
+    print("Error File Already Exist!")
+
 f = open("file1.txt","a")
 f.write("\nThis is the last day of our Training in Python")
 f.close
